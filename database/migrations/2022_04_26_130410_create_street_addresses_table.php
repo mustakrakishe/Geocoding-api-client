3605e629a76479e8f2a55169689dd20c075e6f52
mustakrakishe <mustakrkish@gmail.com>
2022-04-26 16:07:43 +0300

    Create StreetAddress model && migration

---
+++ database/migrations/2022_04_26_130410_create_street_addresses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('street_addresses', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('value');
+            $table->string('place_id')->unique();
+            $table->foreignId('city_id')
+                ->constrained()
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('street_addresses');
+    }
+};
